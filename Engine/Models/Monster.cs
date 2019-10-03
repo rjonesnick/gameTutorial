c4c9bf95663dc7d19fb6dd8fbf162fdd52cd373c
Nicholas Jones <nicholas.jones0604@gmail.com>
2019-10-03 23:03:08 +0200

    10/3

---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Engine.Models
+{
+    public class Monster :  BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; private set; }
+        private string ImageName { get; set; }
+        private int MaximumHitPoints { get; private set; }
+        private int HitPoints
+        {
+            get { return _hitPoints; }
+            private set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName,
+            int maximumHitPoints, int hitPoints,
+            int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = string.Format("/Engine;component/Images/Monsters/{0}", ImageName);
+            MaximumHitPoints = maximumHitPoints;
+            HitPoints = hitPoints;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+            Inventory = new ObservableCollection<ItemQuantity>();
+        }
+    }
+}
