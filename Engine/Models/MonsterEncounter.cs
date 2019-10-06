03a4808c06963d61b96f130f17014557e893325d
Nicholas Jones <nicholas.jones0604@gmail.com>
2019-10-06 21:57:53 +0200

    10/6c

--- Engine/Models/MonsterEncounter.cs
+++ Engine/Models/MonsterEncounter.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-
     public class MonsterEncounter
     {
         public int MonsterID { get; set; }
